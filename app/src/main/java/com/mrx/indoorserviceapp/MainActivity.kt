14021b5442e65fc1839f368ed3d5b4a20071fabf
Vlad <coder.na.r1@gmail.com>
2023-03-05 19:27:02 +0800

    integrate library BeaconManager

--- app/src/main/java/com/mrx/indoorserviceapp/MainActivity.kt
+++ app/src/main/java/com/mrx/indoorserviceapp/MainActivity.kt
@@ -2,10 +2,37 @@ package com.mrx.indoorserviceapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.Observer
+import org.altbeacon.beacon.Beacon
+import org.altbeacon.beacon.BeaconManager
+import org.altbeacon.beacon.Region
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var beaconReferenceApplication: BeaconReferenceApplication
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        beaconReferenceApplication = application as BeaconReferenceApplication
+
+        val region: Region = Region("all-beacons-region", null, null, null)
+        val regionViewModel = BeaconManager.getInstanceForApplication(this).getRegionViewModel(region)
+
+        regionViewModel.rangedBeacons.observe(this, rangingObserver)
+        BeaconManager.getInstanceForApplication(this).startRangingBeacons(region)
+    }
+
+    val rangingObserver = Observer<Collection<Beacon>> { beacons ->
+        Log.d("myTag", "Ranged: ${beacons.count()} beacons")
+    }
+
+    companion object {
+        val TAG = "MyTag"
+        val PERMISSION_REQUEST_BACKGROUND_LOCATION = 0
+        val PERMISSION_REQUEST_BLUETOOTH_SCAN = 1
+        val PERMISSION_REQUEST_BLUETOOTH_CONNECT = 2
+        val PERMISSION_REQUEST_FINE_LOCATION = 3
     }
 }
